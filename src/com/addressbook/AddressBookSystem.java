82eebf7deef715447de67c8d56af13ec7ea7b7b8
jaiswal1nitesh <nj639476@gmail.comgit config --global user.email nj639476@gmail.com>
2023-05-31 16:06:07 +0530

    Added Address Book system

---
+++ src/com/addressbook/AddressBookSystem.java
@@ -0,0 +1,7 @@
+package com.addressbook;
+
+public class AddressBookSystem {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Address Book system");
+    }
+}
